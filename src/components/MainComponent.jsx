d506a3df06a4d67e69bfffa8869930264a3a7949
GRL1000 <gaelrodriguezl474@gmail.com>
2023-11-19 22:26:31 -0600

    Interface finalized

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -8,13 +8,21 @@ function MainComponent() {
             <div className="container">
                 <div className="operations">
                     <label className="enter">Ingresa Función</label>
-                    <input type="text" placeholder="Función"/>
+                    <input type="text"/>
                     <button className="a">Intervalo A</button>
                     <button className="b">Intervalo B</button>
                     <button className="c">CALCULAR</button>
                 </div>
                 <div className="results">
-
+                    <div className="results-n">
+                        <label>Distancia Recorrida</label>
+                        <p>00.00</p>
+                    </div>
+                    <div className="results-b">
+                        <label>Área Total</label>
+                        <p>00.00</p>
+                    </div>
+                    <button className="btn">Nueva Distancia</button>
                 </div>
             </div>
         </div>
